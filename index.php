b0364c2c8bc2c54d637ed420fcdb62e782d3cb1d
sergb <serg.bondarenko109823@mail.ru>
2024-01-12 18:24:18 +0500

    2 lab

--- index.php
+++ index.php
@@ -1,93 +1,48 @@
 <?php
 
-class Product {
+abstract class Product
+{
     public function __construct(
-        private int $id,
-        private string $name,
-        private int $price,
-        private string $description,
-        private string $category) {
-    }
+        protected $price
+    ) { }
 
-    public function getInfo(): string {
-        return "Product: " . $this->name . ", Price: $" . $this->price;
-    }
+    abstract public function getFinalPrice();
+}
 
-    public function setPrice($price): void {
-        $this->price = $price;
+class DigitalProduct extends Product
+{
+    public function getFinalPrice(): int
+    {
+        return $this->price / 2;
     }
 }
 
-class DiscountedProduct extends Product {
+class LogicProduct extends Product
+{
     public function __construct(
-        $id, $name, $price, $description, $category, private int $discount
+        $price,
+        protected $quantity
     ) {
-        parent::__construct($id, $name, $price, $description, $category);
+        parent::__construct($price);
     }
 
-    public function getPriceWithDiscount(): int {
-        return $this->price * (1 - $this->discount);
-    }
-}
-
-class Cart {
-    private $products = [];
-
-    public function addProduct(Product $product): void {
-        $this->products[] = $product;
-    }
-
-    public function removeProduct($productId): void {
-        // logic to remove product by ID
-    }
-
-    public function getTotalPrice(): int {
-        $total = 0;
-        foreach ($this->products as $product) {
-            $total += $product->getPrice();
-        }
-        return $total;
+    public function getFinalPrice(): int
+    {
+        return $this->price * $this->quantity;
     }
 }
 
-class Review {
-
+class SmartProduct extends Product
+{
     public function __construct(
-        private int $id,
-        private string $user,
-        private string $text,
-        private int $rating
-    ) {}
-
-    public function getReviewInfo() {
-        // logic for review information
-    }
-}
-
-class User {
-
-    public function __construct(
-        private int $id,
-        private string $name,
-        private string $email
-    ) { }
-
-    public function register(): void {
-        // user registration logic
+        $price,
+        protected $weight
+    ) {
+        parent::__construct($price);
     }
 
-    public function login(): void {
-        // user login logic
-    }
-}
-
-class FeedbackForm {
-    public function __construct(
-        private string $user,
-        private string $message
-    ) { }
-
-    public function send(): void {
-        // logic to send feedback
+    public function getFinalPrice(): int
+    {
+        return $this->price * $this->weight;
     }
 }
